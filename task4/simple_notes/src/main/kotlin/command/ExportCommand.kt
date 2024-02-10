9d4d025b080ca1acdbbdc043e65de935343950d9
desktop <psomail@yandex.ru>
2024-02-10 10:08:01 +0300

    task4 export command ready

--- task4/simple_notes/src/main/kotlin/command/ExportCommand.kt
+++ task4/simple_notes/src/main/kotlin/command/ExportCommand.kt
@@ -6,6 +6,7 @@ import org.example.exception.NoPersonException
 import org.example.repository.Contacts
 import org.example.util.FileUtils
 import org.example.view.Console
+import java.io.FileNotFoundException
 
 
 class ExportCommand: Command {
@@ -13,28 +14,34 @@ class ExportCommand: Command {
         const val EXPORT_COMPLETED = "export completed"
     }
 
+    override fun isValid(args: String?): Boolean {
+        if (args.isNullOrEmpty() || args.trim().isEmpty()) return false
+        return args?.trim()?.split(" ")?.size == 1
+    }
+
     override fun execute(contacts: Contacts, data: String?) {
         if (!isValid(data)) throw ArgumentErrorException(Command.ARGUMENT_ERROR)
         if (contacts.isEmpty()) throw NoPersonException(Contacts.NOT_INITIALIZED)
 
-        json {
-            jlist {
-                contacts.findAll().forEach{
-                    jobj {
-                        p("name" to it.name)
-                        p("phones" to it.phones)
-                        p("emails" to it.emails)
-                    }
-                }
+        getExportData(contacts).run {
+            try {
+                FileUtils.writeText(data!!, this.toString())
+                Console().output(EXPORT_COMPLETED)
+            } catch (e: FileNotFoundException) {
+                throw ArgumentErrorException(e.stackTraceToString())
             }
-        }.run { FileUtils.writeText(data!!, this.toString())
-            Console().output(EXPORT_COMPLETED)
         }
     }
 
-    override fun isValid(args: String?): Boolean {
-        args.isNullOrEmpty()
-
-        return true
+    private fun getExportData(contacts: Contacts) = json {
+        jlist {
+            contacts.findAll().forEach {
+                jobj {
+                    p("name" to it.name)
+                    p("phones" to it.phones)
+                    p("emails" to it.emails)
+                }
+            }
+        }
     }
 }
